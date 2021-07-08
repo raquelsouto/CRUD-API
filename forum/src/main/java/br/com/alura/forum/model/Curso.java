20ef4733dec0ed81e4cac609c5e8c0bfc41aff35
Raquel Souto <kelsouto@gmail.com>
2021-07-07 21:26:42 -0300

    Atualizar a classe com a anotação do lombok

--- forum/src/main/java/br/com/alura/forum/model/Curso.java
+++ forum/src/main/java/br/com/alura/forum/model/Curso.java
@@ -22,29 +22,4 @@ public class Curso {
 	private Long id;
 	private String nome;
 	private String categoria;
-	
-//	@Override
-//	public int hashCode() {
-//		final int prime = 31;
-//		int result = 1;
-//		result = prime * result + ((id == null) ? 0 : id.hashCode());
-//		return result;
-//	}
-//
-//	@Override
-//	public boolean equals(Object obj) {
-//		if (this == obj)
-//			return true;
-//		if (obj == null)
-//			return false;
-//		if (getClass() != obj.getClass())
-//			return false;
-//		Curso other = (Curso) obj;
-//		if (id == null) {
-//			if (other.id != null)
-//				return false;
-//		} else if (!id.equals(other.id))
-//			return false;
-//		return true;
-//	}
 }
--- forum/src/main/java/br/com/alura/forum/model/Resposta.java
+++ forum/src/main/java/br/com/alura/forum/model/Resposta.java
@@ -30,29 +30,4 @@ public class Resposta {
 	@ManyToOne
 	private Usuario autor;
 	private Boolean solucao = false;
-
-//	@Override
-//	public int hashCode() {
-//		final int prime = 31;
-//		int result = 1;
-//		result = prime * result + ((id == null) ? 0 : id.hashCode());
-//		return result;
-//	}
-//
-//	@Override
-//	public boolean equals(Object obj) {
-//		if (this == obj)
-//			return true;
-//		if (obj == null)
-//			return false;
-//		if (getClass() != obj.getClass())
-//			return false;
-//		Resposta other = (Resposta) obj;
-//		if (id == null) {
-//			if (other.id != null)
-//				return false;
-//		} else if (!id.equals(other.id))
-//			return false;
-//		return true;
-//	}
 }
